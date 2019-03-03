31f88d870b0e312d12d3a80a57a89bfbd605d47d
Jelle <bakker.jelle@outlook.com>
2019-03-03 22:53:44 +0100

    Added juce module wrapper

---
+++ Source/Include.h
@@ -0,0 +1,2 @@
+
+#include "OnePoleZDF.h"
---
+++ WaveguideLibrary.h
@@ -0,0 +1,23 @@
+
+
+/*******************************************************************************
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               WaveguideLibrary
+  vendor:           jbaudio
+  version:          0.0.1
+  name:             Waveguide library
+  description:      Physical modelling waveguide lubrary
+  website:          http://www.jb-audio.com
+  license:          None
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+
+#pragma once
+
+#include "Source/Include.h"

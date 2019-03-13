57bc717ae2714acf612235880fd835e782d976e3
Jelle <bakker.jelle@outlook.com>
2019-03-13 12:22:07 +0100

    inlined LagrangeInterpolation::calculatePoint

--- Source/jbaudio_LagrangeInterpolation.h
+++ Source/jbaudio_LagrangeInterpolation.h
@@ -18,7 +18,7 @@ namespace jbaudio
     public:
         // e.g calculatePoint ({1, 4, 9}, 2.5) returns 2.5 ^ 2 = 6,25
         template <int N>
-        static float calculatePoint (std::array <float, N> points, float p)
+        static inline float calculatePoint (std::array <float, N> points, float p)
         {
             for (int i = 0; i < N; i++)
             {

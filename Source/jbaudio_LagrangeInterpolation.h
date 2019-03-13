c32a2b0d452c5cbe7e8586d5a2819cdb14259a25
Jelle <bakker.jelle@outlook.com>
2019-03-13 12:20:49 +0100

    -

--- Source/jbaudio_LagrangeInterpolation.h
+++ Source/jbaudio_LagrangeInterpolation.h
@@ -16,8 +16,8 @@ namespace jbaudio
     class LagrangeInterpolation
     {
     public:
-        template <int N>
         // e.g calculatePoint ({1, 4, 9}, 2.5) returns 2.5 ^ 2 = 6,25
+        template <int N>
         static float calculatePoint (std::array <float, N> points, float p)
         {
             for (int i = 0; i < N; i++)

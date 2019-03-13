593cb063768294b5799364efc51e901db96f8af9
Jelle <bakker.jelle@outlook.com>
2019-03-13 12:29:01 +0100

    Fixed indentation

--- Source/jbaudio_LagrangeInterpolation.h
+++ Source/jbaudio_LagrangeInterpolation.h
@@ -15,29 +15,31 @@
 
 namespace jbaudio
 {
-    class LagrangeInterpolation
+    
+class LagrangeInterpolation
+{
+public:
+    // e.g calculatePoint ({1, 4, 9}, 2.5) returns 2.5 ^ 2 = 6,25
+    template <int N>
+    static inline float calculatePoint (std::array <float, N> points, float p)
     {
-    public:
-        // e.g calculatePoint ({1, 4, 9}, 2.5) returns 2.5 ^ 2 = 6,25
-        template <int N>
-        static inline float calculatePoint (std::array <float, N> points, float p)
+        for (int i = 0; i < N; i++)
         {
-            for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
             {
-                for (int j = 0; j < N; j++)
+                if (i != j)
                 {
-                    if (i != j)
-                    {
-                        points[i] *= (p - (j + 1)) / ((i + 1) - (j + 1));
-                    }
+                    points[i] *= (p - (j + 1)) / ((i + 1) - (j + 1));
                 }
             }
-            float sum = points[0];
-            for (int i = 1; i < N; i++)
-                sum += points[i];
-            return sum;
         }
-        
-    private:
-    };
+        float sum = points[0];
+        for (int i = 1; i < N; i++)
+            sum += points[i];
+        return sum;
+    }
+    
+private:
+};
+    
 }

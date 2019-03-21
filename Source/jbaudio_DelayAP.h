8e95e00bc3ae462713aa2f7649a4d8e3508883ab
Jelle <bakker.jelle@outlook.com>
2019-03-21 15:24:46 +0100

    Fixed indentation

--- Source/jbaudio_DelayAP.h
+++ Source/jbaudio_DelayAP.h
@@ -14,63 +14,60 @@
 
 namespace jbaudio
 {
-    
-    
-// https://ccrma.stanford.edu/~jos/pasp/First_Order_Allpass_Interpolation.html
-//==============================================================================
-template <int Pow> // 2^Pow
-class DelayAP
-{
-public:
-    DelayAP() {}
-    
-    inline void clear()
+    // https://ccrma.stanford.edu/~jos/pasp/First_Order_Allpass_Interpolation.html
+    //==============================================================================
+    template <int Pow> // 2^Pow
+    class DelayAP
     {
-        array_.fill (0.0f);
-        apZ1_ = 0.0f;
-    }
-    
-    inline void push (float sample)
-    {
-        array_[writeIndex_--] = sample;
-        if (writeIndex_ == -1)
-            writeIndex_ = size_ - 1;
-    }
-    
-    inline void setDelayLength (float samples)
-    {
-        assert (samples >= 1.0f && samples <= size_);
+    public:
+        DelayAP() {}
         
-        apM_ = (int)samples;
-        float fract = samples - apM_;
-        if (fract < 0.1f)
+        inline void clear()
         {
-            fract += 1.0f; // delta range of [0.1, 1.1]
-            apM_ -= 1;
+            array_.fill (0.0f);
+            apZ1_ = 0.0f;
         }
-        apN_ = (1.0f - fract) / (1.0f + fract);
-    }
-    
-    inline float get()
-    {
-        const float a = array_[(writeIndex_ + apM_) & mask_];
         
-        const float b = apZ1_ * -apN_ + a;
-        const float output = b * apN_ + apZ1_;
-        apZ1_ = b;
-        return output;
-    }
-    
-    static constexpr int size_ = ConstexprPow2f <Pow>::value_;
-    
-private:
-    std::array <float, size_> array_ {};
-    static constexpr int mask_ = size_ - 1;
-    int writeIndex_ = 0;
-    
-    int apM_ = 1;
-    float apN_ = 0.0f;
-    float apZ1_ = 0.0f;
-};
-    
+        inline void push (float sample)
+        {
+            array_[writeIndex_--] = sample;
+            if (writeIndex_ == -1)
+                writeIndex_ = size_ - 1;
+        }
+        
+        inline void setDelayLength (float samples)
+        {
+            assert (samples >= 1.0f && samples <= size_);
+            
+            apM_ = (int)samples;
+            float fract = samples - apM_;
+            if (fract < 0.1f)
+            {
+                fract += 1.0f; // delta range of [0.1, 1.1]
+                apM_ -= 1;
+            }
+            apN_ = (1.0f - fract) / (1.0f + fract);
+        }
+        
+        inline float get()
+        {
+            const float a = array_[(writeIndex_ + apM_) & mask_];
+            
+            const float b = apZ1_ * -apN_ + a;
+            const float output = b * apN_ + apZ1_;
+            apZ1_ = b;
+            return output;
+        }
+        
+        static constexpr int size_ = ConstexprPow2f <Pow>::value_;
+        
+    private:
+        std::array <float, size_> array_ {};
+        static constexpr int mask_ = size_ - 1;
+        int writeIndex_ = 0;
+        
+        int apM_ = 1;
+        float apN_ = 0.0f;
+        float apZ1_ = 0.0f;
+    };
 }
